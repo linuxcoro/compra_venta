9937ef0b0e08f665d7519f0a02e027bfcd516a4f
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2017-08-30 08:45:17 -0400

    algo de seguridad

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-{ "express" : { "port" : 80 }, "postgres": { "db" : "linuxcor_symfony", "user" : "linux_linuxcor", "password" : "ne0Jahz2at", "host" : "74.91.28.22" } }
+{ 
+	"db" : "linuxcor_symfony",
+	"user" : "linux_linuxcor",
+	"password" : "ne0Jahz2at",
+	"host" : "74.91.28.22"
+}
